522eacb6918aee50759f5d1bb03ea856a21156c1
DTG-J <grozdanovdt@abv.bg>
2023-07-08 12:44:14 +0300

    Create E801ValidUsernames.java

---
+++ E8TextProcessingExercise/E801ValidUsernames.java
@@ -0,0 +1,33 @@
+package E8TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class E801ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String [] userNames = input.split(", ");
+        for (String username: userNames) {
+            if  (isValidUserName(username)){
+                System.out.println(username);
+            }
+
+
+        }
+
+
+
+
+    }
+    public static boolean isValidUserName (String username) {
+        if (username.length()<3 || username.length()>16){
+            return false;
+        }
+        for (char symbol: username.toCharArray()) {
+            if (!Character.isLetterOrDigit(symbol) && symbol != '-' && symbol != '_'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
