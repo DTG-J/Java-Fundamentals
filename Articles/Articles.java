eb3bddf7cca8111bf7849f085fe3596759c0c677
DTG-J <grozdanovdt@abv.bg>
2023-10-11 18:02:18 +0300

    Create Articles.java

---
+++ Articles/Articles.java
@@ -0,0 +1,35 @@
+package Articles;
+
+public class Articles {
+    private String title;
+    private String content;
+    private String author;
+
+    public Articles(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this. author = author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+    public void edit (String newContent){
+        this.content = newContent;
+    }
+    public void changeAuthor (String newAuthor){
+        this.author = newAuthor;
+    }
+
+    public void rename (String newTitle){
+        this.title = newTitle;
+    }
+}
